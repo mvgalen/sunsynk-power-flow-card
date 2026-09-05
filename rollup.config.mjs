ca3fcd3ea00ce63901139473244ad9ac5027ad91
Marijn van Galen <marijnvangalen@gmail.com>
2026-09-05 23:13:56 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import json from '@rollup/plugin-json';
 
@@ -13,7 +13,10 @@ const plugins = [
     main: true,
   }),
   commonjs(),
-  typescript(),
+  typescript({
+      tsconfig: './tsconfig.json',
+      clean: true
+    }),
   json(),
   babel({
     exclude: 'node_modules/**',
